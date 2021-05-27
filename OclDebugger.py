9c1e38c91a20cf67c12e49a1738418eb3fbaf5b4
LosevMikhail <mikhail.losev.1998@gmail.com>
2021-05-27 13:56:44 +0300

    Build step performed

--- OclDebugger.py
+++ OclDebugger.py
@@ -15,12 +15,13 @@ class OclDebugger(object):
     _break_line: int = None
     _threads: [int] = None
 
-    def __init__(self, kernel_file: str, binary: str):
+    def __init__(self, kernel_file: str, binary: str, build_cmd: str):
         self._kernel_file = kernel_file
         self._binary = os.path.basename(binary)
         self._binary_dir = os.path.dirname(binary)
         # TODO: find out whether it works with Windows
         self._cmd = f'cd {self._binary_dir} && ./{self._binary}'
+        self._build_cmd = build_cmd
 
     def safe_debug(self, break_line: int, threads: [int]):
         self._break_line = break_line
@@ -48,9 +49,13 @@ class OclDebugger(object):
             kernel_file.write(kernel)
             kernel_file.close()
 
-        # TODO: perform the build step somewhere around here
-
         loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(self._build())
+        finally:
+            loop.run_until_complete(loop.shutdown_asyncgens())
+            pass
+
         try:
             variables = loop.run_until_complete(self.process_values(kernel_processor.get_variables(),
                                                                     self.value_generator()))
@@ -86,6 +91,18 @@ class OclDebugger(object):
                 variables.append(Variable(i, v, t))
         return variables
 
+    async def _build(self):
+        if self._build_cmd is not None:
+            env = self._build_env()
+            create = asyncio.create_subprocess_exec(self._build_cmd, env=env,
+                                                     stdin=asyncio.subprocess.PIPE,
+                                                     stdout=asyncio.subprocess.PIPE,
+                                                     stderr=asyncio.subprocess.STDOUT)
+            try:
+                proc = await create
+            except Exception as e:
+                fatal(f'Could not execute {self._binary}')
+
     async def value_generator(self):
         env = self._build_env()
         create = asyncio.create_subprocess_shell(self._cmd, env=env,
@@ -112,13 +129,15 @@ def main():
     parser = argparse.ArgumentParser(prog='OclDebugger.py')
     parser.add_argument('-k', '--kernel', help='<Required> Kernel file location', required=True)
     parser.add_argument('-a', '--application', help='<Required> Target application location', required=True)
-    parser.add_argument('-p', '--breakpoint', help='<Required> Breakpoint', type=int, required=True)
+    parser.add_argument('--build', help='<Optional> build command if needed', type=str, required=False)
+    parser.add_argument('-b', '--breakpoint', help='<Required> Breakpoint', type=int, required=True)
     parser.add_argument('-t', '--threads', nargs='+', help='<Required> Target threads (global ids)', type=int, required=True)
     args = parser.parse_args()
 
     debugger = OclDebugger(
         kernel_file=args.kernel,
-        binary=args.application
+        binary=args.application,
+        build_cmd=args.build
     )
 
     variables = debugger.safe_debug(break_line=args.breakpoint, threads=args.threads)
